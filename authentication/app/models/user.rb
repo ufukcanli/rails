3274440760c5e3080c1b542e889ce48ac437177c
Ufuk Canlı <ufukcanli@hotmail.com>
2025-05-30 10:53:05 +0300

    Add has_secure_password

--- authentication/app/models/user.rb
+++ authentication/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  MINIMUM_PASSWORD_LENGTH = 8
+
+  has_secure_password
+
+  validates :password, length: { minimum: MINIMUM_PASSWORD_LENGTH }
 end
--- authentication/test/models/user_test.rb
+++ authentication/test/models/user_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "password must be at least 8 characters" do
+    invalid_password = "a" * (User::MINIMUM_PASSWORD_LENGTH - 1)
+    user = User.new email: "test@mail.com", password: invalid_password
+
+    assert_not user.valid?
+    assert_includes user.errors.full_messages, "Password is too short (minimum is 8 characters)"
+  end
+
+  test "password must be present" do
+    user = User.new email: "test@mail.com", password: ""
+
+    assert_not user.valid?
+    assert_includes user.errors.full_messages, "Password can't be blank"
+  end
 end
