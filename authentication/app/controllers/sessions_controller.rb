82ff1f4a46e23c47d4f278b24e028111362ae6fe
Ufuk Canlı <ufukcanli@hotmail.com>
2025-05-30 15:20:23 +0300

    Add sign in page

---
+++ authentication/app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if user = User.authenticate_by(session_params)
+      # Add sign-in logic later
+      redirect_to dashboard_path, notice: "Logged in with #{user.email}"
+    else
+      flash.now[:notice] = "Invalid email or password"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
+end
