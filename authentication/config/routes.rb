34cf0a66a3a87f5eebdc2effb0851be724d413fc
Ufuk Canlı <ufukcanli@hotmail.com>
2025-05-30 13:22:56 +0300

    Add #registration page

--- authentication/config/routes.rb
+++ authentication/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resource :dashboard, only: :show
+  resource :registration, only: [:new, :create]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -8,8 +13,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  root "pages#home"
-  resource :dashboard, only: :show
 end
