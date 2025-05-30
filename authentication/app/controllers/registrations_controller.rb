346e6980f1212b75192279fbeed4b804815a42c4
Ufuk Canlı <ufukcanli@hotmail.com>
2025-05-30 13:28:00 +0300

    Add create user action

--- authentication/app/controllers/registrations_controller.rb
+++ authentication/app/controllers/registrations_controller.rb
@@ -2,4 +2,20 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to dashboard_path, notice: "You have successfully registered!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
