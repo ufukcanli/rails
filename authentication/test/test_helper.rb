8e308b70a7c4f0fafacf51e42e041921775a6c37
Ufuk Canlı <ufukcanli@hotmail.com>
2025-06-03 18:46:44 +0300

    Remove require statement and use autoloading instead

--- authentication/test/test_helper.rb
+++ authentication/test/test_helper.rb
@@ -1,7 +1,6 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
-require_relative "test_helpers/session_test_helper"
 
 module ActiveSupport
   class TestCase
