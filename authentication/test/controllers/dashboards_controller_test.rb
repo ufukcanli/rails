4445e0495ec9be835bd09785aeff032161f09034
Ufuk Canlı <ufukcanli@hotmail.com>
2025-06-03 18:34:50 +0300

    Add test for authenticated users

--- authentication/test/controllers/dashboards_controller_test.rb
+++ authentication/test/controllers/dashboards_controller_test.rb
@@ -5,4 +5,14 @@ class DashboardsControllerTest < ActionDispatch::IntegrationTest
     get dashboard_path
     assert_redirected_to new_session_path
   end
+
+  test "should get #show" do
+    sign_in users(:ufuk)
+    get dashboard_path
+    assert_response :success
+  end
+
+  def sign_in(user)
+    post session_path, params: { session: { email: user.email, password: "password" } }
+  end
 end
