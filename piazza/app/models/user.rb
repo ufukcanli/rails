686d3ce49cd48d928aec71de15f374957f30da54
ufukcanli <ufukcanli@hotmail.com>
2025-06-10 15:24:18 +0300

    Add relationships

--- piazza/app/models/user.rb
+++ piazza/app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   validates :email,
               format: { with: URI::MailTo::EMAIL_REGEXP },
               uniqueness: { case_sensitive: false }
+
+  has_many :memberships, dependent: :destroy
+  has_many :organizations, through: :memberships
 end
