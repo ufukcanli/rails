c9a72e2f969289b2f04f5f34ea5e32e013de31d9
ufukcanli <ufukcanli@hotmail.com>
2025-06-10 13:47:53 +0300

    Add user model tests that fail

--- piazza/test/models/user_test.rb
+++ piazza/test/models/user_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires a name" do
+    @user = User.new(name: "", email: "johndoe@example.com")
+    assert_not @user.valid?
+
+    @user.name = "John"
+    assert @assert.valid?
+  end
+
+  test "requires a valid email" do
+    @user = User.new(name: "John", email: "")
+    assert_not @user.valid?
+
+    @user.email = "invalid"
+    assert_not @user.valid?
+
+    @user.email = "johndoe@example.com"
+    assert @user.valid?
+  end
+
+  test "requires a unique email" do
+    @existing_user = User.create(name: "John", email: "jd@example.com")
+    assert @existing_user.persisted?
+
+    @user = User.new(name: "Jon", email: "jd@example.com")
+    assert_not @user.valid?
+  end
 end
