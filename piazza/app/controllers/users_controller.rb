2030cf380120b2ab02883fe0161a097f2d73667d
Ufuk Canlı <ufukcanli@hotmail.com>
2025-06-12 20:51:23 +0300

    Add signup page and locales

--- piazza/app/controllers/users_controller.rb
+++ piazza/app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      @organization = Organization.create(members: [ @user ])
+      # TODO: Log in user
+      redirect_to root_path, status: :see_other, flash: { success: t(".welcome", name: @user.name) }
+    end
   end
+
+  private
+
+    def user_params
+      params
+        .require(:user)
+        .permit(:name, :email, :password)
+    end
 end
