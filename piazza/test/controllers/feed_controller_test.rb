083263239501b3a540f976b86b93b49816e09227
Ufuk Canlı <ufukcanli@hotmail.com>
2025-06-12 22:17:20 +0300

    Add users controller tests

--- piazza/test/controllers/feed_controller_test.rb
+++ piazza/test/controllers/feed_controller_test.rb
@@ -1,8 +1,4 @@
 require "test_helper"
 
 class FeedControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get feed_show_url
-    assert_response :success
-  end
 end
--- piazza/test/controllers/users_controller_test.rb
+++ piazza/test/controllers/users_controller_test.rb
@@ -1,13 +1,40 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
-    assert_response :success
+  test "redirects to feed after successful sign up" do
+    get sign_up_path
+    assert_response :ok
+
+    assert_difference [ "User.count", "Organization.count" ], 1 do
+      post sign_up_path, params: {
+        user: {
+          name: "John",
+          email: "johndoe@example.com",
+          password: "password"
+        }
+      }
+    end
+
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select ".notification.is-success", text: I18n.t("users.create.welcome", name: "John")
   end
 
-  test "should get create" do
-    get users_create_url
-    assert_response :success
+  test "renders errors if input data is invalid" do
+    get sign_up_path
+    assert_response :ok
+
+    assert_no_difference [ "User.count", "Organization.count" ] do
+      post sign_up_path, params: {
+        user: {
+          name: "John",
+          email: "johndoe@example.com",
+          password: "pass"
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+    assert_select "p.is-danger", text: I18n.t("activerecord.errors.models.user.attributes.password.too_short")
   end
 end
