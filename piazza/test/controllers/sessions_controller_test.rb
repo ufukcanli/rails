e45149dd2ed301a3e79ea3c6d95379033b1500c4
ufukcanli <ufukcanli@hotmail.com>
2025-06-13 10:50:32 +0300

    Add sessions controller tests

---
+++ piazza/test/controllers/sessions_controller_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:jerry)
+  end
+
+  test "user is logged in and redirected to home with correct credentials" do
+    assert_difference("@user.app_sessions.count", 1) {
+      post login_path, params: {
+        user: {
+          email: "jerry@example.com",
+          password: "password"
+        }
+      }
+    }
+
+    assert_redirected_to root_path
+  end
+
+  test "error is rendered for login with incorrect credentials" do
+    post login_path, params: {
+      user: {
+        email: "wrong@example.com",
+        password: "password"
+      }
+    }
+
+    assert_select ".notification", I18n.t("sessions.create.incorrect_details")
+  end
+end
