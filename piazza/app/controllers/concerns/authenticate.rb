598a2f7b162c49cf73a93c935b50e84b6338aadd
ufukcanli <ufukcanli@hotmail.com>
2025-06-13 13:01:35 +0300

    Add other authentication utilities

--- piazza/app/controllers/concerns/authenticate.rb
+++ piazza/app/controllers/concerns/authenticate.rb
@@ -1,5 +1,5 @@
 module Authenticate
-  # extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
   protected
 
@@ -7,5 +7,26 @@ module Authenticate
       cookies.encrypted.permanent[:app_session] = {
         value: app_session.to_h
       }
+    end
+
+  private
+
+    def authenticate
+      Current.app_session = authenticate_using_cookie
+      Current.user = Current.app_session&.user
+    end
+
+    def authenticate_using_cookie
+      app_session = cookies.encrypted[:app_session]
+      authenticate_using app_session&.with_indifferent_access
+    end
+
+    def authenticate_using(data)
+      data => { user_id:, app_session:, token: }
+
+      user = User.find_by(user_id)
+      user.authenticate_app_session(app_session, token)
+    rescue NoMatchingPatternError, ActiveRecord::RecordNotFound
+      nil
     end
 end
