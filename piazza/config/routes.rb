10547d2746765a471cecda9e60c39eef8242d42b
ufukcanli <ufukcanli@hotmail.com>
2025-06-13 08:54:08 +0300

    Add login page

--- piazza/config/routes.rb
+++ piazza/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get "sign_up", to: "users#new"
   post "sign_up", to: "users#create"
 
+  get "login", to: "sessions#new"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
