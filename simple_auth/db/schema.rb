f709fbd611d0841a80e9be543834b833bc337f65
Ufuk Canlı <ufukcanli@hotmail.com>
2025-06-17 18:45:23 +0300

    Generate account model
    https://github.com/ufukcanli/rails.git

--- simple_auth/db/schema.rb
+++ simple_auth/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_17_093533) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_17_154316) do
+  create_table "accounts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
